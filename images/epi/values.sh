1fc2422fbef97e64d71b6aa2c8c641a7c9091de4
Nicu-Cosmin Ursache <asac.cool@gmail.com>
2022-04-20 16:10:16 +0300

    added tag script

--- images/epi/values.sh
+++ images/epi/values.sh
@@ -1,5 +1,6 @@
 HUB_IDENTIFIER='docker.io'
 BUILDER_NAME='epi-builder'
 BUILDER_REPO_NAME='pharmaledger/epi-builder'
+VERSION='1.0.0'
 RUNNER_NAME='epi-runner'
 RUNNER_REPO_NAME='pharmaledger/epi-runner'
