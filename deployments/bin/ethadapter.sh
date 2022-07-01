745c3df5917b4947dc1169d28620d3a955860ef6
skutner <sava.dumitru.daniel@gmail.com>
2022-07-01 13:29:56 +0300

    small fix in ethadapter.sh

--- deployments/bin/ethadapter.sh
+++ deployments/bin/ethadapter.sh
@@ -27,5 +27,5 @@ fi
 
 echo "network_name: \"$NETWORK_NAME\"" > $COMPANY_NAME/tmp/networkName.yaml
 
-helm pl-plugin --ethAdapter -i $ethValuesPath $ghInfoPath $ethServicePath $qnInfoPath $smartContractInfoPath $ethInfoPath $COMPANY_NAME/tmp/ethadapter-values.yaml $COMPANY_NAME/tmp/rpc-address.yaml $COMPANY_NAME/tmp/networkName.yaml -o $COMPANY_NAME/tmp
-helm upgrade --install --wait --timeout=600s ethadapter pharmaledger-imi/ethadapter -f $ethServicePath -f $COMPANY_NAME/tmp/ethadapter-values.yaml -f $COMPANY_NAME/tmp/rpc-address.yaml -f $COMPANY_NAME/tmp/networkName.yaml --set-file config.smartContractInfo=$COMPANY_NAME/tmp/eth-adapter.plugin.json,secrets.orgAccountJson=$COMPANY_NAME/tmp/orgAccount.json
+helm pl-plugin --ethAdapter -i $ethValuesPath $ghInfoPath $ethServicePath $qnInfoPath $smartContractInfoPath $ethInfoPath $ethInfoPath $COMPANY_NAME/tmp/ethadapter-values.yaml $COMPANY_NAME/tmp/rpc-address.yaml $COMPANY_NAME/tmp/networkName.yaml -o $COMPANY_NAME/tmp
+helm upgrade --install --wait --timeout=600s ethadapter pharmaledger-imi/ethadapter -f $ethServicePath -f $ethInfoPath -f $COMPANY_NAME/tmp/ethadapter-values.yaml -f $COMPANY_NAME/tmp/rpc-address.yaml -f $COMPANY_NAME/tmp/networkName.yaml --set-file config.smartContractInfo=$COMPANY_NAME/tmp/eth-adapter.plugin.json,secrets.orgAccountJson=$COMPANY_NAME/tmp/orgAccount.json
