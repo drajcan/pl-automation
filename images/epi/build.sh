7ff9679f1b2e3d463efaab8a21276eacf47d8897
Sava Daniel <35995331+skutner@users.noreply.github.com>
2022-10-14 15:31:50 +0300

    Update build.sh

--- images/epi/build.sh
+++ images/epi/build.sh
@@ -1,18 +1,18 @@
-#git clone http://github.com/pharmaledger-imi/epi-workspace tmp/epi
+git clone http://github.com/pharmaledger-imi/epi-workspace tmp/epi
 cd tmp/epi
 . ../../values.sh
-#if [ "$BUILD_TYPE" == "dev" ]; then
-#  echo "npm run dev-install"
-#  npm run dev-install
-#else
-#  echo "npm install"
-#  npm install
-#fi
-#
-#node ./node_modules/octopus/scripts/setEnv --file=../../../env.json "node ./bin/octopusRun.js postinstall"
+if [ "$BUILD_TYPE" == "dev" ]; then
+  echo "npm run dev-install"
+  npm run dev-install
+else
+  echo "npm install"
+  npm install
+fi
+
+node ./node_modules/octopus/scripts/setEnv --file=../../../env.json "node ./bin/octopusRun.js postinstall"
 cd ../../
 
 DOCKER_BUILDKIT=1 docker build --no-cache -t $BUILDER_REPO_NAME --build-arg BASE_IMAGE=$NODE_ALPINE_BASE_IMAGE . -f builder-dockerfile  --network host
 DOCKER_BUILDKIT=1 docker build --no-cache -t $RUNNER_REPO_NAME --build-arg BASE_IMAGE=$NODE_ALPINE_BASE_IMAGE . -f runner-dockerfile  --network host
 
-#rm -rf tmp
+rm -rf tmp
