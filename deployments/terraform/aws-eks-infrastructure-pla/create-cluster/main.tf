170ed4d05d8e6157289668199c3066de9f1aed18
skutner <sava.dumitru.daniel@gmail.com>
2023-01-25 15:11:48 +0200

    small fix

--- deployments/terraform/aws-eks-infrastructure-pla/create-cluster/main.tf
+++ deployments/terraform/aws-eks-infrastructure-pla/create-cluster/main.tf
@@ -5,7 +5,7 @@ module "cluster" {
   # IMPORTANT: The source can be either
   # a) a copy of this repository into your own repo (local copy), e.g. to `modules/aws-eks-infrastructure` - `source = "./modules/aws-eks-infrastructure"`
   # b) a copy of this repository into a dedicated repo of your choice. See https://developer.hashicorp.com/terraform/language/modules/sources for configuring the module source.
-  source = ".." # for the example, we reference the module in this repo
+  source = "../" # for the example, we reference the module in this repo
 
   eks_cluster_name                          = var.eks_cluster_name
   account_id                                = var.account_id
