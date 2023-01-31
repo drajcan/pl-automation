c2f986db67244df367850636424d7ec211986811
skutner <sava.dumitru.daniel@gmail.com>
2023-01-31 14:24:35 +0200

    changed scripts path

--- deployments/terraform/aws-eks-bootstrap-pla/bootstrap-cluster/main.tf
+++ deployments/terraform/aws-eks-bootstrap-pla/bootstrap-cluster/main.tf
@@ -2,7 +2,7 @@ module "bootstrapping" {
   # IMPORTANT: The source can be either
   # a) a copy of this repository into your own repo (local copy), e.g. to `modules/aws-eks-bootstrapping` - `source = "./modules/aws-eks-bootstrapping"`
   # b) a copy of this repository into a dedicated repo of your choice. See https://developer.hashicorp.com/terraform/language/modules/sources for configuring the module source.
-  source = "../.." # for the example, we reference the module in this repo
+  source = "../" # for the example, we reference the module in this repo
 
   region              = var.region
   eks_cluster_name    = var.eks_cluster_name
