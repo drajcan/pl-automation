85aa9f540da43da200f0ab9542c498fffec0df7c
skutner <sava.dumitru.daniel@gmail.com>
2023-02-09 21:51:19 +0200

    updated getPartners.sh

--- deployments/bin/utils/getPartners.sh
+++ deployments/bin/utils/getPartners.sh
@@ -1,10 +1,13 @@
 NETWORKS_FOLDER=$1
 NETWORK_NAME=$2
+CLUSTER_NAME=$3
 # shellcheck disable=SC2207
 folderPaths=($(find $NETWORKS_FOLDER -path "*/editable/$NETWORK_NAME" | cut -f 3 -d "/"))
 partners="["
 for i in "${!folderPaths[@]}"; do
-  partners="$partners'${folderPaths[$i]}',"
+  if [ ! "${folderPaths[$i]}" == $CLUSTER_NAME ]; then
+    partners="$partners'${folderPaths[$i]}',"
+  fi
 done
 if  [ ! "$partners" == "[" ]; then
   partners=$(echo "$partners" | rev | cut -c2- | rev)
